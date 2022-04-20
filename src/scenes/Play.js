192c58bbfd403c195489e4e0185d16d9bbdf5b62
Peyton <projectile212@gmail.com>
2022-04-19 21:51:48 -0700

    Added basic files for setting up the Repo.

---
+++ src/scenes/Play.js
@@ -0,0 +1,19 @@
+class Play extends Phaser.Scene {
+    constructor(){
+        super("playScene");
+    }
+
+    preload() {
+        //examples
+        //this.load.image('rocket', './assets/rocket.png');
+        //this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+        
+      }
+
+    create(){
+
+    }
+    update() {
+
+    }
+}
